8c31116e896a4dc1ba9696a5c914752b2c3c3e1c
Pablo Juan Belaustegui <pbelaustegui@gmail.com>
2011-07-27 14:16:47 -0400

    first commit

---
+++ src/main/groovy/ar/edu/utn/elecciones/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package ar.edu.utn.elecciones
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
