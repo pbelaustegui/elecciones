61a7f8c2cfc150e34222715090269f489e9b5835
Pablo Juan Belaustegui <pbelaustegui@gmail.com>
2011-07-27 22:02:00 -0300

    completo el test

--- src/test/groovy/ar/edu/utn/elecciones/LegisladoresTestCase.groovy
+++ src/test/groovy/ar/edu/utn/elecciones/LegisladoresTestCase.groovy
@@ -1,24 +1,29 @@
 package ar.edu.utn.elecciones;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 
 class LegisladoresTestCase {
-	private pro, pj, ucr, po, resultado
+	def pro, pj, ucr, po, resultado, bancas
 	
 	@Before
 	def void init(){
+		bancas = 30
 		pro = new Lista(716532, "pro")
 		pj = new Lista(556932, "pj")
 		ucr = new Lista(45693, "ucr")
 		po = new Lista(17345, "po")
-		resultado = new ResultadoLegisladores(30, [pro,pj,ucr, po])
+		resultado = new ResultadoLegisladores(bancas, [pro,pj,ucr, po])
 	}
 	
 	@Test
 	def void testResultado(){
 		resultado.resultado().each {unBloque -> println(unBloque) }
+		Assert.assertEquals(bancas, resultado.resultado().inject( 0,
+			{suma, unBloque -> suma + unBloque.cantidadBancas() }) )
+		
 	}
 
 }
