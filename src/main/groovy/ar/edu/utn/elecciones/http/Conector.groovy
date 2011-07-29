bf88a72ce27d6646b8a00289d8cbf6f3868e226a
Pablo Juan Belaustegui <pbelaustegui@gmail.com>
2011-07-29 18:49:42 -0400

    probando con las regexp

---
+++ src/main/groovy/ar/edu/utn/elecciones/http/Conector.groovy
@@ -0,0 +1,35 @@
+package ar.edu.utn.elecciones.http
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+class Conector {
+	
+	def url
+	def HttpMethod method
+	def HttpClient client
+	public Conector(Object url) {
+		super();
+		this.url = url;
+		this.client = new HttpClient()
+		this.method = new GetMethod(url);
+	}
+	
+	def getBody(){
+		def body
+		try {
+			client.executeMethod(method)
+			body = new String(method.getResponseBody());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			method.releaseConnection()
+		}
+		return body
+	}
+	
+	def getResultados(){
+		return this.getBody().replaceAll("\n", "")
+	}
+}
--- src/test/groovy/ar/edu/utn/elecciones/LegisladoresTestCase.groovy
+++ src/test/groovy/ar/edu/utn/elecciones/LegisladoresTestCase.groovy
@@ -1,12 +1,16 @@
 package ar.edu.utn.elecciones;
 
+import java.util.regex.Matcher;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import ar.edu.utn.elecciones.http.Conector;
+
 
 class LegisladoresTestCase {
-	def pro, pj, ucr, po, resultado, bancas
+	def pro, pj, ucr, po, resultado, bancas, conector
 	
 	@Before
 	def void init(){
@@ -16,13 +20,28 @@ class LegisladoresTestCase {
 		ucr = new Lista(45693, "ucr")
 		po = new Lista(17345, "po")
 		resultado = new ResultadoLegisladores(bancas, [pro,pj,ucr, po])
+		conector = new Conector("http://www.eleccionescaba2011.gob.ar/paginas/paginas/dat01/DGO01999.htm")
 	}
 	
-	@Test
+//	@Test
 	def void testResultado(){
 		resultado.resultado().each {unBloque -> println(unBloque) }
 		Assert.assertEquals(bancas, resultado.resultado().inject( 0,
 			{suma, unBloque -> suma + unBloque.cantidadBancas() }) )
+		
+	}
+	
+	@Test
+	def void testConector(){
+		def resultado = conector.getBody()
+		def matcher = (resultado =~ /<t[hd].*>(.*)<\/t[hd].*>/)
+		matcher.each {p-> println p[1] }
+	}
+	
+//	@Test
+	def void testSarlanga(){
+		def Matcher matcher = ("abctotototabctototoabc" =~ /(a)b(c)/)
+		matcher.each {p-> println p }
 		
 	}
 
